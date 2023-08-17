6c4c2f3625e62ba85c7c127ce6fd5f5b7e61b888
Yusuf CEYLAN <yusuf.ceylann@outlook.com.tr>
2023-08-17 17:37:59 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace karakter_tersten_yazdirma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Metni giriniz: ");
+            string inputValue = Console.ReadLine();
+
+            Console.WriteLine("Metnin tersine çevrilmiş hali: " + ReverseStringValue(inputValue));
+        }
+
+        public static string ReverseStringValue(string input)
+        {
+            string[] inputArray = input.Split(' ');
+            string output = "";
+
+            foreach (var item in inputArray)
+            {
+                char[] charArray = item.ToCharArray();
+                Array.Reverse(charArray);
+
+                string reverseValue = new string(charArray);
+
+                output += reverseValue + " ";
+            }
+            return output;
+        }
+    }
+}
